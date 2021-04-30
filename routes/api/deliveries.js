3d6380d7e11e80315c0d9227362245a6553612ac
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-04-30 16:06:22 -0700

    stubs up api/routes, controllers and deliveries-api

---
+++ routes/api/deliveries.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const deliveriesCtrl = require('../../controllers/api/deliveries');
+
+/* GET /api/deliveries */
+
+module.exports = router;
