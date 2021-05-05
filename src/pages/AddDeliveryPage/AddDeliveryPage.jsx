14006f5e6582670198d537dd6889165a665c46d5
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-05-05 00:53:38 -0700

    adds final css to deliveries/new page

--- src/pages/AddDeliveryPage/AddDeliveryPage.jsx
+++ src/pages/AddDeliveryPage/AddDeliveryPage.jsx
@@ -32,36 +32,41 @@ export default function AddDeliveryPage({ handleAddDelivery } ) {
   };
   return(
     <>
-       <h1 className="newDeliveryPageTitle">Add a Delivery to your Route</h1>
+       <h1 className="newDeliveryPageTitle">Add a Delivery to Your Route</h1>
        <span>Disclaimer: Make sure the inputted info matches the package label for each package!</span>
-   
+   <div className="newFormContainer">
     <Form className="NewDeliveryForm" autoComplete="off" ref={formRef} onSubmit={handleSubmit}>
-      <Form.Row>
-        <Form.Group as={Col} controlId="formGridName">
+      <Form.Row className="newFormRow">
+        <Form.Group as={Col} controlId="formGridName" className='newNameInp' >
           <Form.Label className="newFormLabel">Recipient Name</Form.Label>
           <Form.Control type="name" placeholder="John Smith" name="recipName"
              value={formData.recipName} onChange={handleChange}
             required />
         </Form.Group>
 
-        <Form.Group as={Col} controlId="formGridNumber">
-          <Form.Label className="newFormLabel">Package Weight (lbs)</Form.Label>
+        <Form.Group as={Col} controlId="formGridNumber" className='newNumInp' >
+          <Form.Label className="newFormLabel"> Item Count</Form.Label>
+          <Form.Control type="number" placeholder="1" name="itemQty" value={formData.itemQty} onChange={handleChange}/>
+        </Form.Group>
+
+        <Form.Group as={Col} controlId="formGridNumber" className='newNumInp' >
+          <Form.Label className="newFormLabel"> Weight (lbs)</Form.Label>
           <Form.Control type="number" placeholder="5" name="weight" value={formData.weight} onChange={handleChange}/>
         </Form.Group>
       </Form.Row>
 
-      <Form.Group controlId="formGridAddress1">
+      <Form.Group controlId="formGridAddress1" className='newAddressInp'>
         <Form.Label className="newFormLabel">Recipient Address</Form.Label>
         <Form.Control placeholder="1234 Main St" />
       </Form.Group>
 
-      <Form.Group controlId="formGridAddress2">
+      <Form.Group controlId="formGridAddress2"  className='newAddressInp' >
         <Form.Label className="newFormLabel">Address 2</Form.Label>
         <Form.Control placeholder="Apartment, studio, or floor" />
       </Form.Group>
 
-    <Form.Row>
-      <Form.Group as={Col} controlId="formGridCity">
+    <Form.Row className="newFormRow">
+      <Form.Group as={Col} controlId="formGridCity"  className='newSelect' >
         <Form.Label className="newFormLabel">City</Form.Label>
         <Form.Control as="select" defaultValue="Choose..." select value={formData.location} className="form-control" name="location" onChange={handleChange}>
           <option>Select...</option>
@@ -76,7 +81,7 @@ export default function AddDeliveryPage({ handleAddDelivery } ) {
         </Form.Control>
       </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridState">
+      <Form.Group as={Col} controlId="formGridState"  className='newSelect' >
         <Form.Label className="newFormLabel">State</Form.Label>
         <Form.Control as="select" defaultValue="Choose...">
           <option>Select...</option>
@@ -84,13 +89,13 @@ export default function AddDeliveryPage({ handleAddDelivery } ) {
         </Form.Control>
       </Form.Group>
 
-      <Form.Group as={Col} controlId="formGridZip">
+      <Form.Group as={Col} controlId="formGridZip"  className='newZipInp' >
         <Form.Label className="newFormLabel">Zip</Form.Label>
-        <Form.Control placeholder="1234" />
+        <Form.Control placeholder="12345" />
       </Form.Group>
     </Form.Row>
-    <Form.Row> 
-        <Form.Group as={Col} controlId="formGridCity">
+    <Form.Row className="newFormRow"> 
+        <Form.Group as={Col} controlId="formGridCity"  className='newStatus' >
         <Form.Label className="newFormLabel">Delivery Status</Form.Label>
         <Form.Control as="select" defaultValue="Select..." value={formData.status} className="form-control" name="status" 
                     onChange={handleChange}>
@@ -99,12 +104,13 @@ export default function AddDeliveryPage({ handleAddDelivery } ) {
           <option>In Route</option>
         </Form.Control>
       </Form.Group>
-    </Form.Row>
 
-    <Button variant="primary" type="submit"  disabled={invalidForm}>
-      Submit
-    </Button>
-  </Form>
-    </>
+      <Button className="newBtn" variant="primary" type="submit"  disabled={invalidForm}>
+        Submit
+      </Button>
+      </Form.Row>
+      </Form>
+    </div>
+  </>
   );
 }
