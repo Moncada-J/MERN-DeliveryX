3c233f044bcd9b1ae7e5156b51715bef37f10420
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-05-06 15:11:33 -0700

    reverses package json change

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "./index.css";
 import 'bootstrap/dist/css/bootstrap.min.css';
+import "mdb-react-ui-kit/dist/css/mdb.min.css";
 import App from "./pages/App/app";
 
 ReactDOM.render(
