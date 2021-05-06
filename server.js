e05d39bfa4d98b52b1b955e9f794ab8f751ffc16
Jessalynn Moncada <moncadaj@Jessalynns-MacBook-Air.local>
2021-05-05 21:22:47 -0700

    updates server

--- server.js
+++ server.js
@@ -13,22 +13,22 @@ const deliveriesRouter = require("./routes/api/deliveries");
 const app = express();
 
 app.use(logger('dev'));
-app.use(express.json());
-
 app.use(favicon(path.join(__dirname, 'build', 'favicon.ico')));
+app.use(express.json());
 app.use(express.static(path.join(__dirname, 'build')));
 
 // Middleware to verify token and assign user object of payload to req.user
 // Be sure to mount before our routes
 app.use(require('./config/checkToken'));
 
+app.use('/api/deliveries', require('./routes/api/deliveries'));
 app.use('/api/users', require('./routes/api/users'));
-app.use("/api/deliveries", deliveriesRouter);
+
 
 //api routes will go here
 // app.use('/api/users', require('./routes/api/users'))
 const ensureLoggedIn = require("./config/ensureLoggedIn");
-app.use('/api/deliveries', ensureLoggedIn, require('./routes/api/deliveries'))
+app.use('/api/deliveries', ensureLoggedIn, require('./routes/api/deliveries'));
 
 // The following "catch all" route (note the *) is necessary
 app.get('/*', function (req, res) {
@@ -37,6 +37,7 @@ app.get('/*', function (req, res) {
 
 // Configure to use port 3001 instead of 3000 during development to avoid collision with React's dev server
 const port = process.env.PORT || 3001;
+
 app.listen(port, function () {
 	console.log(`Express app running on port ${port}`);
 });
